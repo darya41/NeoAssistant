a5a28b95a46234eb6006f5d5dafe44f6a83e5120
daraermak41@gmail.com <daraermak41@gmail.com>
2026-02-05 08:17:38 +0300

    feat: add IconWidgets class for icon components

---
+++ lib/utils/icon_widgets.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class IconWidgets {
+  static Widget infoIcon({
+    required BuildContext context,
+    required VoidCallback onTap,
+    double size = 24,
+    Color backgroundColor = const Color(0xFFE0E0E0),
+    Color iconColor = Colors.grey,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: backgroundColor,
+        ),
+        child: Icon(
+          Icons.info_outline,
+          size: size * 0.67,
+          color: iconColor,
+        ),
+      ),
+    );
+  }
+
+  static Widget visibilityIcon({
+    required bool isVisible,
+    required VoidCallback onTap,
+    double size = 20,
+    Color color = Colors.grey,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Icon(
+        isVisible ? Icons.visibility_off : Icons.visibility,
+        color: color,
+        size: size,
+      ),
+    );
+  }
+}
