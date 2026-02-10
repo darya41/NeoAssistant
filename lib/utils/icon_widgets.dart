207e479a13dcc06baf5c67ca4149d4ab782a3b87
daraermak41@gmail.com <daraermak41@gmail.com>
2026-02-10 12:23:46 +0300

    feat: add new icon

--- lib/utils/icon_widgets.dart
+++ lib/utils/icon_widgets.dart
@@ -1,26 +1,30 @@
 import 'package:flutter/material.dart';
 
 class IconWidgets {
+  static const double _defaultIconSize = 24.0;
+  static const double _smallIconSize = 20.0;
+  static const double _largeIconSize = 26.0;
+
+  static const _defaultBackgroundColor = Color(0xFFE0E0E0);
+  static const _iconColor = Colors.black;
+
   static Widget infoIcon({
     required BuildContext context,
     required VoidCallback onTap,
-    double size = 24,
-    Color backgroundColor = const Color(0xFFE0E0E0),
-    Color iconColor = Colors.grey,
   }) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        width: size,
-        height: size,
+        width: _defaultIconSize,
+        height: _defaultIconSize,
         decoration: BoxDecoration(
           shape: BoxShape.circle,
-          color: backgroundColor,
+          color: _defaultBackgroundColor,
         ),
         child: Icon(
           Icons.info_outline,
-          size: size * 0.67,
-          color: iconColor,
+          size: _defaultIconSize,
+          color: _iconColor,
         ),
       ),
     );
@@ -29,23 +33,19 @@ class IconWidgets {
   static Widget visibilityIcon({
     required bool isVisible,
     required VoidCallback onTap,
-    double size = 20,
-    Color color = Colors.grey,
   }) {
     return GestureDetector(
       onTap: onTap,
       child: Icon(
-        isVisible ? Icons.visibility_off : Icons.visibility,
-        color: color,
-        size: size,
+        isVisible ? Icons.visibility_off_outlined : Icons.visibility_outlined,
+        color: _iconColor,
+        size: _defaultIconSize,
       ),
     );
   }
 
   static Widget searchIcon({
     required VoidCallback onTap,
-    double size = 20,
-    Color color = Colors.grey,
     EdgeInsetsGeometry padding = EdgeInsets.zero,
   }) {
     return GestureDetector(
@@ -54,10 +54,68 @@ class IconWidgets {
         padding: padding,
         child: Icon(
           Icons.search,
-          color: color,
-          size: size,
+          color: _iconColor,
+          size: _smallIconSize,
         ),
       ),
     );
   }
+
+  static Widget micIcon({
+    required VoidCallback onTap,
+    EdgeInsetsGeometry padding = EdgeInsets.zero,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: padding,
+        child: Icon(
+          Icons.mic_none_sharp,
+          size: _largeIconSize ,
+          color: _iconColor,
+        ),
+      ),
+    );
+  }
+
+  static Widget filterIcon({
+    required VoidCallback onTap,
+    EdgeInsetsGeometry padding = EdgeInsets.zero,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: padding,
+        child: Icon(
+          Icons.filter_list,
+          size: _smallIconSize,
+          color: _iconColor,
+        ),
+      ),
+    );
+  }
+
+  static Widget addIcon({
+    required VoidCallback onTap,
+    Color backgroundColor = Colors.white,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: _defaultIconSize,
+        height: _defaultIconSize,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: backgroundColor,
+        ),
+        child: Icon(
+          Icons.add,
+          size: _defaultIconSize,
+          color: _iconColor,
+        ),
+      ),
+    );
+  }
+
+
 }
