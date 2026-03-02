7dcbc27c3ddbaa4ea7deacb663c9be87c0df7b28
daraermak41@gmail.com <daraermak41@gmail.com>
2026-03-02 11:04:50 +0300

    feat: add loading screen

---
+++ lib/features/auth/ presentation/pages/loading_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToLogin();
+  }
+
+  _navigateToLogin() async {
+    await Future.delayed(const Duration(seconds: 2));
+    if (mounted) {
+      Navigator.pushReplacementNamed(context, '/login');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'assets/images/loading.png',
+          width: 200,
+          height: 200,
+          fit: BoxFit.contain
+        ),
+      ),
+    );
+  }
+}
