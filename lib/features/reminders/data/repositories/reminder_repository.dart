c7229c552a42dacfc510a4951a97b4ba86eec3d4
daraermak41@gmail.com <daraermak41@gmail.com>
2026-03-16 20:46:56 +0300

    feat: add ReminderRepository with all CRUD methods

--- lib/features/reminders/data/repositories/reminder_repository.dart
+++ lib/features/reminders/data/repositories/reminder_repository.dart
@@ -1,4 +1,5 @@
 import '../../../../core/network/api_client.dart';
+import '../../../../models/reminder.dart';
 
 class ReminderRepository {
   Future<Map<String, dynamic>> getRemindersStats() async {
@@ -29,6 +30,75 @@ class ReminderRepository {
       return '${date.day} ${months[date.month - 1]}';
     } catch (e) {
       return '';
+    }
+  }
+
+  Future<List<Reminder>> getReminders() async {
+    try {
+      final response = await ApiClient.getAuth('reminders');
+      final List<dynamic> data = response['data'] ?? [];
+      return data.map((json) => Reminder.fromJson(json)).toList();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> toggleReminderStatus(int reminderId, bool isCompleted) async {
+    try {
+      await ApiClient.putAuth('reminders/$reminderId', {
+        'is_completed': isCompleted ? 1 : 0,
+      });
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> deleteReminder(int reminderId) async {
+    try {
+      await ApiClient.deleteAuth('reminders/$reminderId');
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Reminder> getReminderById(int id) async {
+    try {
+      final response = await ApiClient.getAuth('reminders/$id');
+      return Reminder.fromJson(response['data']);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> markAsCompleted(int id) async {
+    try {
+      await ApiClient.putAuth('reminders/$id', {
+        'is_completed': 1,
+      });
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Reminder> createReminder({
+    required String title,
+    String? description,
+    required DateTime date,
+  }) async {
+    try {
+      final response = await ApiClient.postAuth('reminders', {
+        'title': title,
+        'description': description,
+        'reminder_date': date.toIso8601String().split('T')[0],
+      });
+
+      if (response['success'] == true) {
+        return Reminder.fromJson(response['data']);
+      } else {
+        throw Exception(response['error'] ?? 'Ошибка создания напоминания');
+      }
+    } catch (e) {
+      rethrow;
     }
   }
 }
