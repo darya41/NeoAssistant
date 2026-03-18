e737928e8e2722f0d9989a4c1d69dcc4cfa208f4
daraermak41@gmail.com <daraermak41@gmail.com>
2026-03-18 13:51:39 +0300

    feat: add mother and address models

---
+++ lib/models/address.dart
@@ -0,0 +1,80 @@
+class Address {
+  final int id;
+  final String? settlementType;
+  final String? city;
+  final String? addressType;
+  final String? street;
+  final String? houseNumber;
+  final String? apartment;
+  final String? building;
+
+  const Address({
+    required this.id,
+    this.settlementType,
+    this.city,
+    this.addressType,
+    this.street,
+    this.houseNumber,
+    this.apartment,
+    this.building,
+  });
+
+  factory Address.fromJson(Map<String, dynamic> json) {
+    return Address(
+      id: json['address_id'] ?? 0,
+      settlementType: json['settlement_type'],
+      city: json['city'],
+      addressType: json['address_type'],
+      street: json['street'],
+      houseNumber: json['house_number'],
+      apartment: json['apartment'],
+      building: json['building'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'settlement_type': settlementType,
+      'city': city,
+      'address_type': addressType,
+      'street': street,
+      'house_number': houseNumber,
+      'apartment': apartment,
+      'building': building,
+    };
+  }
+
+  String get fullAddress {
+    final parts = <String>[];
+
+    // Населенный пункт
+    if (settlementType != null && city != null) {
+      parts.add('$settlementType $city');
+    } else if (city != null) {
+      parts.add(city!);
+    }
+
+    // Улица
+    if (street != null && street!.isNotEmpty) {
+      final streetPart = addressType != null ? '$addressType $street' : 'ул. $street';
+      parts.add(streetPart);
+    }
+
+    // Дом
+    if (houseNumber != null && houseNumber!.isNotEmpty) {
+      parts.add('д. $houseNumber');
+    }
+
+    // Корпус
+    if (building != null && building!.isNotEmpty) {
+      parts.add('корп. $building');
+    }
+
+    // Квартира
+    if (apartment != null && apartment!.isNotEmpty) {
+      parts.add('кв. $apartment');
+    }
+
+    return parts.isNotEmpty ? parts.join(', ') : 'Адрес не указан';
+  }
+}
---
+++ lib/models/mother.dart
@@ -0,0 +1,70 @@
+class Mother {
+  final int id;
+  final String firstName;
+  final String lastName;
+  final String? patronymic;
+  final DateTime dateOfBirth;
+  final int? numberOfDeliveries;
+  final int? numberOfPregnancies;
+  final String? medicalHistory;
+  final String? medicationsDuringPregnancy;
+  final bool? gestationalDiabetes;
+  final bool? preeclampsia;
+  final String? groupBStreptococcusStatus;
+  final int? addressId;
+
+  String get fullName => [lastName, firstName, patronymic]
+      .where((name) => name != null && name.isNotEmpty)
+      .join(' ');
+
+  const Mother({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    this.patronymic,
+    required this.dateOfBirth,
+    this.numberOfDeliveries,
+    this.numberOfPregnancies,
+    this.medicalHistory,
+    this.medicationsDuringPregnancy,
+    this.gestationalDiabetes,
+    this.preeclampsia,
+    this.groupBStreptococcusStatus,
+    this.addressId,
+  });
+
+  factory Mother.fromJson(Map<String, dynamic> json) {
+    return Mother(
+      id: json['mother_id'],
+      firstName: json['first_name'],
+      lastName: json['last_name'],
+      patronymic: json['patronymic'],
+      dateOfBirth: DateTime.parse(json['date_of_birth']),
+      numberOfDeliveries: json['number_of_deliveries'],
+      numberOfPregnancies: json['number_of_pregnancies'],
+      medicalHistory: json['medical_history'],
+      medicationsDuringPregnancy: json['medications_during_pregnancy'],
+      gestationalDiabetes: json['gestational_diabetes'] == 1,
+      preeclampsia: json['preeclampsia'] == 1,
+      groupBStreptococcusStatus: json['groupb_streptococcus_status'],
+      addressId: json['address_id'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'first_name': firstName,
+      'last_name': lastName,
+      'patronymic': patronymic,
+      'date_of_birth': dateOfBirth.toIso8601String().split('T')[0],
+      'number_of_deliveries': numberOfDeliveries,
+      'number_of_pregnancies': numberOfPregnancies,
+      'medical_history': medicalHistory,
+      'medications_during_pregnancy': medicationsDuringPregnancy,
+      'gestational_diabetes': gestationalDiabetes == true ? 1 : 0,
+      'preeclampsia': preeclampsia == true ? 1 : 0,
+      'groupb_streptococcus_status': groupBStreptococcusStatus,
+      'address_id': addressId,
+    };
+  }
+}
